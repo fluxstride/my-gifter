051ce6c767b717d744b3a55f099056d6ee658066
Samuel Adekoya <samueladexx@gmail.com>
2024-06-17 19:44:25 +0100

    linting: setup linting with airbnb recommended configs and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,17 +5,31 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:all",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/strict',
+    'plugin:@typescript-eslint/stylistic',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { project: 'tsconfig.json' },
+  plugins: ['react-refresh'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
+    'react-refresh/only-export-components': [
+      'warn',
       { allowConstantExport: true },
     ],
+    'prettier/prettier': [
+      'error',
+      {},
+      {
+        usePrettierrc: true,
+      },
+    ],
+    'import/extensions': 'off',
   },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import "./App.css";
+import './App.css';
 
-const App = () => <h1>My Gifter</h1>;
+function App() {
+  return <h1>My Gifter</h1>;
+}
 
 export default App;
