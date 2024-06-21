dea3c02021ff2a9240b3e25555a81451f8cb0ce0
osain paul <osainpaul87@gmail.com>
2024-06-21 11:11:01 +0100

    Feat: added a picker feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import './App.css';
+import NumberList from './components/Pickpage';
 
 function App() {
-  return <h1>My Gifter</h1>;
+  return (
+    <>
+      <h1>My Gifter</h1>
+      <NumberList />
+    </>
+  );
 }
 
 export default App;
