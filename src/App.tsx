04cd603c0911067d39d30c0a6d2ab654c9b0faa1
Konboye Austine <konboyeaustine@gmail.com>
2024-06-25 19:36:56 +0100

    chore: updated styling for code page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import './App.css';
+import CodePage from './pages/CodePage';
 
 function App() {
-    return <h1>Gifter</h1>;
+  // return <h1>Gifter</h1>;
+  return <CodePage />;
 }
 
 export default App;
