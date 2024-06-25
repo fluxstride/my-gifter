abed72f64ed07a4aeaa44cc786e8cdc1b5c02468
Konboye Austine <konboyeaustine@gmail.com>
2024-06-25 16:25:44 +0100

    chore: updated styling for code page

--- src/pages/CodePage.tsx
+++ src/pages/CodePage.tsx
@@ -1,67 +1,27 @@
 import './CodePage.css';
+import giftImage from './logo/image.png';
 
 function CodePage() {
-  /* const [number, setNumber] = useState('');
-  const [error, setError] = useState('');
-  const navigate = useNavigate();
-
-  const handleChange = event => {
-    setNumber(event.target.value);
-    setError('');
-  };
-  const handleSubmit = async event => {
-    event.preventDefault();
-
-    if (!number) {
-      setError('Number is required');
-      return;
-    }
-
-    try {
-      // Replace with your backend endpoint
-      const response = await axios.post('/api/validate-number', { number });
-
-      if (response.data.exists) {
-        navigate('/different-page');
-      } else {
-        setError('Number does not exist in the database');
-      }
-    } catch (err) {
-      if (err.response) {
-        // Server responded with a status other than 200 range
-        setError(`Server error: ${err.response.data.message}`);
-      } else if (err.request) {
-        // Request was made but no response received
-        setError('Network error: Please try again later');
-      } else {
-        // Something else happened while setting up the request
-        setError(`Error: ${err.message}`);
-      }
-    }
-  };
-*/
   return (
     <div>
-      <div className="intro-container">
-        <h1>
-          {' '}
-          ATUKU-OWEI /MUMMY QUEEN DYNASTY AND IN-LAW&apos;S YEAR 2024 FAMILY
-          GIFTING{' '}
-        </h1>
+      <div className="header">
+        <img src={giftImage} className="image" alt="gift" />
+        <p>My Gifter</p>
+      </div>
+      <div className="gift-title">
+        <h1>2024 FAMILY GIFTING</h1>
       </div>
       <div className="form-container">
         <form>
           <label htmlFor="code">
-            Enter Code:
-            <input
-              placeholder="Enter Code Here.."
-              type="number"
-              id="code"
-              required
-            />
+            Enter Code
+            <input placeholder="...#123" type="number" id="code" required />
           </label>
           <button type="submit">Submit</button>
         </form>
+      </div>
+      <div className="footer">
+        <p>ATUKU-OWI/QUEEN DYNASTY AND IN LAWS</p>
       </div>
     </div>
   );
