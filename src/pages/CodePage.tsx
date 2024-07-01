b254780458a47baf63e87ba844382785ebf79cfe
Konboye Austine <konboyeaustine@gmail.com>
2024-07-01 12:10:16 +0100

    updated design

--- src/pages/CodePage.tsx
+++ src/pages/CodePage.tsx
@@ -4,25 +4,39 @@ import giftImage from './logo/image.png';
 function CodePage() {
   return (
     <div>
-      <div className="header">
-        <img src={giftImage} className="image" alt="gift" />
-        <p>My Gifter</p>
-      </div>
-      <div className="gift-title">
-        <h1>2024 FAMILY GIFTING</h1>
-      </div>
-      <div className="form-container">
-        <form>
-          <label htmlFor="code">
-            Enter Code
-            <input placeholder="...#123" type="number" id="code" required />
-          </label>
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-      <div className="footer">
-        <p>ATUKU-OWI/QUEEN DYNASTY AND IN LAWS</p>
-      </div>
+      <header>
+        <div className="header">
+          <img src={giftImage} className="image" alt="gift" />
+          <p>My Gifter</p>
+        </div>
+      </header>
+      <main>
+        <div className="gift-title">
+          <h1>2024 FAMILY GIFTING</h1>
+        </div>
+        <div className="form-container">
+          <form className="code-form">
+            <label className="code-label" htmlFor="code">
+              Enter Code
+              <input
+                className="code-input"
+                placeholder="...#123"
+                type="number"
+                id="code"
+                required
+              />
+            </label>
+            <button className="code-submit-btn" type="submit">
+              Submit
+            </button>
+          </form>
+        </div>
+      </main>
+      <footer>
+        <div className="footer">
+          <p>ATUKU-OWI/QUEEN DYNASTY AND IN LAWS</p>
+        </div>
+      </footer>
     </div>
   );
 }
